297bf2b912989ace9da00b219d84a187d3076087
Subash Pathak <Subash@users.noreply.github.com>
2020-06-03 09:35:20 +0545

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,18 +1,20 @@
 const { toAD, toBS } = require('../src');
 
-test('toBS()', ()=> {
-  expect(toBS({ year: 1943, month: 4, day: 14 })).toEqual({ year: 2000, month: 1, day: 1 });
-  expect(toBS({ year: 1943, month: 4, day: 15 })).toEqual({ year: 2000, month: 1, day: 2 });
+test.only('toBS()', ()=> {
+  expect(toBS({ year: 1893, month: 4, day: 12 })).toEqual({ year: 1950, month: 1, day: 1 });
+  expect(toBS({ year: 1893, month: 4, day: 13 })).toEqual({ year: 1950, month: 1, day: 2 });
   expect(toBS({ year: 2015, month: 9, day: 20 })).toEqual({ year: 2072, month: 6, day: 3 });
-  expect(()=> toBS({ year: 1943, month: 4, day: 13 })).toThrow();
+  expect(()=> toBS({ year: 1893, month: 4, day: 11 })).toThrow();
   expect(()=> toBS({ year: 2070, month: 1, day: 1 })).toThrow();
 });
 
-test('toAD()', ()=> {
-  expect(toAD({ year: 2000, month: 1, day: 1 })).toEqual({ year: 1943, month: 4, day: 14 });
+test.only('toAD()', ()=> {
+  expect(toAD({ year: 1976, month: 9, day: 16 })).toEqual({ year: 1919, month: 12, day: 31 });
+  expect(toAD({ year: 1976, month: 9, day: 17 })).toEqual({ year: 1920, month: 1, day: 1 });
   expect(toAD({ year: 2000, month: 1, day: 2 })).toEqual({ year: 1943, month: 4, day: 15 });
   expect(toAD({ year: 2072, month: 6, day: 3 })).toEqual({ year: 2015, month: 9, day: 20 });
-  expect(()=> toAD({ year: 1999, month: 1, day: 1 })).toThrow();
+  expect(toAD({ year: 2089, month: 12, day: 30 })).toEqual({ year: 2033, month: 4, day: 13 });
+  expect(()=> toAD({ year: 1949, month: 1, day: 1 })).toThrow();
   expect(()=> toAD({ year: 2000, month: 1, day: 33 })).toThrow();
-  expect(()=> toAD({ year: 2100, month: 1, day: 33 })).toThrow();
+  expect(()=> toAD({ year: 2100, month: 1, day: 1 })).toThrow();
 });
